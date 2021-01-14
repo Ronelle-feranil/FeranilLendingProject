a9ba9e638c73b38eca77f756902c0c746625cf74
Ronelle Feranil <ronelle.feranil@mynt.xyz>
2021-01-15 00:13:33 +0800

    main screen

--- FeranilLendingProject/Form1.vb
+++ FeranilLendingProject/Form1.vb
@@ -2,4 +2,10 @@
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
         Me.Close()
     End Sub
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        main.Show()
+        Me.Hide()
+
+    End Sub
 End Class
